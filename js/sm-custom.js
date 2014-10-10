d1a0100b448468048480ba939cf36dbd71162bb8
Pedro Miguel Sombreireiro <pedro.sombreireiro@saphety.com>
2014-10-10 21:08:09 +0100

    First commit

---
+++ js/sm-custom.js
@@ -0,0 +1,60 @@
+ // ClickOut Function
+
+(function($) {
+    $.fn.saphOnClickOut = function(funcToExecute) {
+
+        var element = $(this);
+        var eventHandlerName = 'click.saphOnClickOut' + ((new Date().getTime()));
+        $(document).bind(eventHandlerName, function(e) {
+            var $clicked = $(e.target);
+            if (!($clicked.is(element) || $clicked.parents().is(element))) {
+                funcToExecute(element);
+                $(document).unbind(eventHandlerName);
+                e.stopPropagation();
+            }
+        });
+    }
+})(jQuery);
+
+
+ // Header
+
+$('[data-nav="nav-item"]').on('click', function(){
+
+	if($(this).parent().hasClass('is-active'))
+	{
+		$(this).parent().removeClass('is-active');
+		$('[data-nav="sub-menu"]').removeClass('is-visible');
+
+	} else
+	{
+		$('[data-nav="nav-item"]').parent().removeClass('is-active');
+		$(this).parent().addClass('is-active');
+		$('[data-nav="sub-menu"]').addClass('is-visible')
+	}
+
+});
+
+
+
+
+
+
+// Custom alerts
+
+$('[data-action="delete-item"]').on('click', function(e){
+        e.preventDefault();
+
+        swal({
+        title: "Are you sure?",
+        text: "These changes cannot be undone",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        closeOnConfirm: false
+    },
+    function(){
+        swal("Deleted!", "The item was sucessfully deleted.", "success");
+    });
+});
